55aa09ceba10dcc821643d2adb8e83fc7145a4e9
valbar <io@valeriobarbuto.eu>
2022-07-01 17:10:00 +0200

    removed WebSecurityConfigurerAdapter

--- config-server/src/main/java/com/example/config/server/WebSecurityConfig.java
+++ config-server/src/main/java/com/example/config/server/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.example.config.server;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+public class WebSecurityConfig {
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
     }
 }
